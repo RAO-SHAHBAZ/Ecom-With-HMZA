f04a2087a58867b20bfe15379e0ae082149bb276
Rao SHahbaz <menlenon59@gmail.com>
2025-04-19 14:52:55 +0500

    changed Bg Color On Contact US PAge

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='flex items-center justify-center h-[100vh] w-full'>
-        <h1 className='text-red-300 p-9 border border-amber-200 text-6xl'>
+    <div className='flex items-center justify-center h-[100vh] w-full bg-slate-400'>
+        <h1 className='text-white p-9 border border-amber-200 text-6xl'>
             Contact US
         </h1>
     </div>
