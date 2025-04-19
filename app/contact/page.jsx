92de5caf0eb83259b25019423926f6aa98a0ab00
Rao SHahbaz <menlenon59@gmail.com>
2025-04-19 14:47:18 +0500

    Contact Us PAge Created Succesfuly

---
+++ app/contact/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex items-center justify-center h-[100vh] w-full'>
+        <h1 className='text-red-300 p-9 border border-amber-200 text-6xl'>
+            Contact US
+        </h1>
+    </div>
+  )
+}
+
+export default page
