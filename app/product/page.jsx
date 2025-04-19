c2e928d6e284983e72ed4978ccf380062eef4f19
hamza <jreemree@gmail.com>
2025-04-19 15:02:50 +0500

    product page add kiya hy

---
+++ app/product/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const product = () => {
+  return (
+    <div className='bg-red-400 text-white font-bold'>
+      <h1>produ</h1>
+    </div>
+  )
+}
+
+export default product
