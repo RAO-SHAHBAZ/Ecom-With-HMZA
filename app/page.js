123744f23fde3f25284cb949a95717d4603041fa
hamza <jreemree@gmail.com>
2025-04-19 14:30:17 +0500

    update home icon from react icon

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaHome } from "react-icons/fa";
+
 
 function page() {
   return (
     <div>
       <h1 className='text-red-400 text-6xl'>
         GITHUB COLLABRATIONS
+        <FaHome />
+
+
       </h1>
     </div>
   )
