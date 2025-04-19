483b6080cf3d4e4bd7ff72cba3562a2bcc98a6a4
hamza <jreemree@gmail.com>
2025-04-19 14:38:52 +0500

    About US PAge Created Succesfully

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex justify-center items-center bg-black text-white h-[100vh] w-full '>
+      <h1 className='text-5xl border-amber-300 border p-10'>
+      ABOUT US
+      </h1>
+    </div>
+  )
+}
+
+export default page
