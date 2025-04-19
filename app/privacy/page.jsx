a84744fe38a47d8157ef28aa5c9bdb5630d47aed
Rao SHahbaz <menlenon59@gmail.com>
2025-04-19 15:07:29 +0500

    Privacy Policy PAge Added

---
+++ app/privacy/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex items-center justify-center h-[100vh] w-full bg-slate-400'>
+        <h1 className='text-white p-9 border border-amber-200 text-6xl'>
+            Privacy Policy
+        </h1>
+    </div>
+  )
+}
+
+export default page
